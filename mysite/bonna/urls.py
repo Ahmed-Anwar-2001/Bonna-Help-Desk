98ec17f5b11a57b57a3cde34c6ffbf66487aedf8
Ahmed-Anwar-2001 <ahmedanwar.aa872@gmail.com>
2024-08-23 17:07:14 +0600

    Add Data

--- mysite/bonna/urls.py
+++ mysite/bonna/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-
-
     path('', home, name='home'),
+    path('add_district', add_district, name='add_district'),
+    path('add_union_ward', add_union_ward, name='add_union_ward'),
+    path('get_upozillas/<int:district_id>/', get_upozillas, name='get_upozillas'),
 ]
--- mysite/bonna/views.py
+++ mysite/bonna/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
 
+from.models import *
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+
+@csrf_protect
+def add_district(request):
+    if request.method == 'POST':
+        district_name = request.POST.get('district_name')
+        upozilla_names = request.POST.getlist('upozilla_names[]')
+
+        # Check if the district name already exists
+        if District.objects.filter(name=district_name).exists():
+            print("District with this name already exists")
+            return render(request, 'add_district.html')  # Redisplay the form with an error message
+
+        if district_name:
+            district = District.objects.create(name=district_name)
+
+            for upozilla_name in upozilla_names:
+                if upozilla_name:
+                    Upozilla.objects.create(name=upozilla_name, district=district)
+
+            return redirect(home)  # Redirect to a success page after submission
+
+    return render(request, 'add_district.html')
+
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
+from .models import District, Upozilla, UnionWard
+from django.contrib import messages
+
+@csrf_protect
+def add_union_ward(request):
+    if request.method == 'POST':
+        district_id = request.POST.get('district_id')
+        upozilla_id = request.POST.get('upozilla_id')
+        union_names = request.POST.getlist('union_names[]')
+
+        if district_id and upozilla_id:
+            for union_name in union_names:
+                if union_name:
+                    UnionWard.objects.create(
+                        name=union_name,
+                        district_id=district_id,
+                        upozilla_id=upozilla_id
+                    )
+            return redirect(home)  
+        
+    districts = District.objects.all()
+    return render(request, 'add_union_ward.html', {'districts': districts})
+
+def get_upozillas(request, district_id):
+    upozillas = Upozilla.objects.filter(district_id=district_id)
+    data = {'upozillas': [{'id': u.id, 'name': u.name} for u in upozillas]}
+    return JsonResponse(data)
